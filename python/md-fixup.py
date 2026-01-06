b5d0e9907cf2bd07b3a4ffb72754c1547b01661c
Brett Terpstra <me@brettterpstra.com>
2026-01-06 07:55:22 -0600

    Bump version to 0.1.6

--- python/md-fixup.py
+++ python/md-fixup.py
@@ -42,7 +42,7 @@ try:
 except ImportError:
     yaml = None
 
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 DEFAULT_WRAP_WIDTH = 60
 
 # Valid GitHub emoji names (normalized: lowercase, hyphens to underscores, sorted, deduplicated)
